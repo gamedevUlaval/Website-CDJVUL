94f008b8ce0ac4290d8588dadee6efbb9d428dcc
MaximePlourde <98863560+Crazytrain1@users.noreply.github.com>
2024-09-08 18:20:34 -0400

    Reapply "updated president info, picture and site version"
    This reverts commit eda21a149290c1c456572cd81123fec4ebff29f9.

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -112,7 +112,7 @@
               </a>
               <br>
               <a
-                href="https://discord.gg/w5waVkPKTj"
+                href="https://discord.gg/yFQpaspDFV"
                 target="_blank"
               >
                 <div class="d-flex align-items-center justify-content-center">
@@ -145,8 +145,8 @@
               :src="presidentImage"
             >
             <br><br>
-            <p>Emmanuel Coutlée</p>
-            <p>Étudiant à la maîtrise en Intelligence Artificielle</p>
+            <p>Maxime Plourde</p>
+            <p>Étudiant au Baccalauréat en informatique</p>
           </div>
         </div>
       </div>
@@ -177,7 +177,7 @@ import * as projectsDataFile from "../projects/Projects.js";
 import discordIcon from '@/assets/home/discord.jpg';
 import facebookIcon from '@/assets/home/Facebook.svg.png';
 import logoPath from '@/assets/home/logo_cdjvul_1920x1080.png';
-import presidentImage from '@/assets/home/emmanuel_coutlee.jpg';
+import presidentImage from '@/assets/home/maxime_plourde.jpg';
 
 export default {
   name: "HomePage",
